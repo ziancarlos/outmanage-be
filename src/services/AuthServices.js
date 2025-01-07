6f5b894b242a652b6cdf01c2c6a4102aeeded789
Zian <ziancrlswong@gmail.com>
2025-01-07 18:03:02 +0700

    commit

--- src/services/AuthServices.js
+++ src/services/AuthServices.js
@@ -2,8 +2,8 @@ import ResponseError from "../errors/ResponseError.js";
 import {
   loginValidation,
   refreshValidation,
-} from "../validations/AuthValidations.js";
-import validate from "../validations/validation.js";
+} from "../Validations/AuthValidations.js";
+import validate from "../Validations/validation.js";
 import sanitize from "sanitize-html";
 import bcrypt from "bcrypt";
 import prismaClient from "../utils/Database.js";
