37b063ff90f6be63bfebb0c1954a5a5051a79a59
Zian <ziancrlswong@gmail.com>
2025-01-26 22:18:22 +0700

    commit

--- src/controllers/AuthControllers.js
+++ src/controllers/AuthControllers.js
@@ -7,6 +7,8 @@ async function login(req, res, next) {
     const { userId, username, accessToken, refreshToken, roleId } =
       await AuthService.login(body, req.ip);
 
+    console.log(refreshToken);
+
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: false,
@@ -14,8 +16,6 @@ async function login(req, res, next) {
       sameSite: false,
       maxAge: 24 * 60 * 60 * 1000,
     });
-
-    console.log(refreshToken);
 
     res.status(200).json({
       data: { userId, username, accessToken, roleId },
@@ -47,12 +47,11 @@ async function logout(req, res, next) {
 async function refresh(req, res, next) {
   try {
     const refreshToken = req.cookies.refreshToken;
+    console.log(refreshToken);
 
     const { userId, username, accessToken, roleId } = await AuthService.refresh(
       refreshToken
     );
-
-    console.log(refreshToken);
 
     res.status(200).json({
       data: {
