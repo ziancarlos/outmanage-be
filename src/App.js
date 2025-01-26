c13f7365d5bbc5b122896bc355ba162982fab0e5
Zian <ziancrlswong@gmail.com>
2025-01-26 21:52:01 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ app.use(
     origin: "http://localhost:3000", // Replace with your frontend's origin (if it's local, use this)
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Allow these HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allow these headers
+    credentials: true, // Allow sending cookies with requests
   })
 );
 
