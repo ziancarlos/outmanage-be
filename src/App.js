9ae44de1484bae38f154debfb03556a6552a940e
Zian <ziancrlswong@gmail.com>
2025-04-06 00:13:12 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -18,23 +18,23 @@ app.use(cookieParser());
 
 const origin = process.env.ALLOW_ORIGIN || "https://outmanage.online";
 
-// app.use(
-//   cors({
-//     origin, // Allow requests from this origin
-//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], // Allow these HTTP methods
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin, // Allow requests from this origin
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], // Allow these HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use(express.json());
 
